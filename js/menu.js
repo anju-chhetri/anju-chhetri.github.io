7ea352b5fa9a83d760db3757fbbbe2af267e5aa6
anju-chhetri <anju9chhetri@gmail.com>
2026-06-01 14:45:58 +0545

    CV, Bib, AISN

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,9 @@
-$(function(){
-    var $page = jQuery.url.attr("file");
-    $('ul.navigation li a').each(function(){
-        var $href = $(this).attr('href');
-        if ( ($href == $page) || ($href == '') ) {
-            $(this).addClass('on');
-        } else {
-            $(this).removeClass('on');
-        }
-    });
-});
+function toggleBib(button) {
+    const bibtex = button.nextElementSibling;
+
+    if (bibtex.style.display === "none") {
+        bibtex.style.display = "block";
+    } else {
+        bibtex.style.display = "none";
+    }
+}
